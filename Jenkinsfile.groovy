4c7b7503274ed21f5401bd850421005b945271b5
navaneethan2 <naveenhari55@gmail.com>
2021-10-15 12:13:18 +0530

    first commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,11 +5,17 @@ def totest(){
 pipeline{
     agent any
 
+    environment {
+        buildComponents = 'build-components.json'
+    }
+
     stages{
         stage('debug'){
             steps{
                 script{
                     totest()
+                    builds = load 'builds.groovy'
+                    builds.catJson()
                 }
             }
         }
---
+++ builds.groovy
@@ -0,0 +1,8 @@
+def getBuildComponentsBySublist(String sublist) {
+
+    return sh(script: "cat ${buildComponents} | jq -r .'applications.sublists[] | select(.name == \"${sublist}\") | .repos[] | select(.enabled == true) | .repo_name'", returnStdout: true)
+}
+
+def catJson(){
+    return sh(script: "cat ${buildComponents}" ,returnStdout: true)
+}
